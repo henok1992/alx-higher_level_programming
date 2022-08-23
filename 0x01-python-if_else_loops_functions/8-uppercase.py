19e58b0334d02c5376949b45df862822db37a39a
henok1992 <henokgithub@gmail.com>
2022-08-23 18:19:20 +0300

    ahblv

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
+#!/usr/bin/python3
+def uppercase(str):
+    temp = ""
+    for i in range(len(str)):
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            temp = temp + chr(ord(str[i]) - 32)
+        else:
+            temp = temp + str[i]
+    print("{}".format(temp))
