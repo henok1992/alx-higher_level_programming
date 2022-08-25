edab2dadb8b46d56fb601d6c030cd2702ccb4b82
henok1992 <henok1992@gmail.com>
2022-08-25 16:46:39 +0300

    cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-if __name__ == '__main__':
-    if len(argv) - 1 != 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+if __name__ == "__main__":
+    import calculator_1 as cal
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
-    if argv[2] not in '+-*/':
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
-    if argv[2] == '+':
-        print('{} + {} = {}'.format(a, b, add(a, b)))
-    elif argv[2] == '-':
-        print('{} - {} = {}'.format(a, b, sub(a, b)))
-    elif argv[2] == '*':
-        print('{} * {} = {}'.format(a, b, mul(a, b)))
-    elif argv[2] == '/':
-        print('{} / {} = {}'.format(a, b, div(a, b))
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if (sys.argv[2] == "+"):
+            print("{} + {} = {}".format(a, b, cal.add(a, b)))
+            exit(0)
+        elif (sys.argv[2] == "-"):
+            print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+            exit(0)
+        elif (sys.argv[2] == "*"):
+            print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+            exit(0)
+        elif (sys.argv[2] == "/"):
+            print("{} / {} = {}".format(a, b, cal.div(a, b)))
+            exit(0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
