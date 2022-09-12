3f6ca7701ffebc5530c27e8f7d126721a55256fd
henok1992 <henok1992@gmail.com>
2022-09-12 18:53:26 +0300

    qhfC

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    """ Returns True if value has been correctly printed
+(it means the value is an integer)"""
     try:
         print("{:d}".format(value))
-    except:
-        return (False)
-    else:
-        return (True)
+        return True
+    except (ValueError, TypeError):
+        return False
