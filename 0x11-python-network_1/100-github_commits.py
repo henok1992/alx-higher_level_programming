d95ce8786a59602b9c7c0c5088c966712adba0cd
henok1992 <henokgithub@gmail.com>
2022-12-16 04:03:55 -0800

    kwjge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-Python script that shows the last 10 commits of a repository
-in GitHub
+Python script that list 10 commits (from the most recent to oldest) of the repository
 """
-from requests import get, auth
-import sys
 
+import requests
+from sys import argv
 
 if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    res = requests.get(url)
+    commits = res.json()
     try:
-        repo = sys.argv[1]
-        owner = sys.argv[2]
-        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-        r = get(url)
-        json_o = r.json()
-        for i in range(0, 10):
-            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
-                                  .get('author').get('name')))
-    except:
+        for i in range(10):
+            sha = commits[i].get('sha')
+            author = commits[i].get('commit').get('author').get('name')
+            print("{}: {}".format(sha, author))
+    except IndexError:
         pass
