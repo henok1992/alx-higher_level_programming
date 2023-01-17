9848f433099ba8a5e8e3901bb7647ecafc369694
henok1992 <henokgithub@gmail.com>
2023-01-17 16:20:22 +0000

    whfbqe

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,7 @@
 #!/usr/bin/node
-/**
- * Script that reads from a file
- */
-const process = require('process');
 const fs = require('fs');
-
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) { console.log(error); } else { console.log(data); }
 });
