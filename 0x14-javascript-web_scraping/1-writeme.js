218e28df08af2b4dc06a55ebec2a35edcc22d73a
henok1992 <henokgithub@gmail.com>
2023-01-17 16:16:40 +0000

    avg

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-/**
- * Script that writes a string to a file
- */
-
-const process = require('process');
 const fs = require('fs');
-
 const filePath = process.argv[2];
-const content = process.argv[3];
+const writeData = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', err => {
-    if (err) {
-        console.log(err);
-    }
+fs.writeFile(filePath, writeData, 'utf-8', (error) => {
+  if (error) { console.log(error); }
 });
