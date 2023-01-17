5c10e626299166cdea543df3d3f0a5bb31083e72
henok1992 <henokgithub@gmail.com>
2023-01-17 15:59:39 +0000

    wjkGC

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,28 +1,16 @@
 #!/usr/bin/node
-/**
- * Script that prints all characters of a Star Wars movie
- */
-
 const request = require('request');
-const process = require('process');
-const filmId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-request.get(url, (err, _resp, body) => {
-    if (err === null) {
-        const film = JSON.parse(body);
-        const characters = film.characters;
-        characters.forEach((character) => {
-            request.get(character, (charError, response, body) => {
-                if (err === null) {
-                    const characterInfo = JSON.parse(body);
-                    console.log(characterInfo.name);
-                } else {
-                    console.log(charError);
-                }
-            });
-        });
-    } else {
-        console.log(err);
-    }
+request(url, (error, response, body) => {
+  if (error) { console.log(error); }
+  const result = JSON.parse(body);
+  for (const charURL of result.characters) {
+    request(charURL, (err, r, body) => {
+      if (err) { console.log(err); }
+      const result = JSON.parse(body);
+      console.log(result.name);
+    });
+  }
 });
